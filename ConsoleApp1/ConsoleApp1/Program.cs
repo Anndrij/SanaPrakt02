21c1db3804cb03e722e14e3d2be5b3cfc84e7f71
Shevcuk Andrey <andrijsevcuk201@gmail.com>
2022-11-11 15:54:52 +0200

    first coomit in branch text

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("hello");
+Console.WriteLine("Rik");
+Console.WriteLine("and Morty");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
